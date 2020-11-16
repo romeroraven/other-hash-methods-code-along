4a90d4857884206e0f9d58ab8e3386de6eb1ceaf
romeroraven <romeroravenr@gmail.com>
2020-11-16 03:49:11 -0700

    Done.

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,8 +1,3 @@
-require 'pry'
-
-#This is the groceries hash we'll be passing in to the get_the_keys method
- 
-
 def get_the_keys(groceries)
   groceries_keys = [:dairy, :vegetable, :meat, :grains]
   
